dcd615e4a7f9d4af4c8e6c51e7a6460149857bf0
ogtslag <developitl@gmail.com>
2025-06-04 22:28:40 -0600

    Version funcional

--- src/main/java/com/hmm/test/infraestructure/adapter/web/PaymentsController.java
+++ src/main/java/com/hmm/test/infraestructure/adapter/web/PaymentsController.java
@@ -1,26 +1,23 @@
 package com.hmm.test.infraestructure.adapter.web;
 
 import com.hmm.test.domain.model.Payment;
-import com.hmm.test.infraestructure.adapter.persistence.MongoPaymentRepositoryAdapter;
+import com.hmm.test.domain.port.PaymentRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @AllArgsConstructor
 @RequestMapping("/api/payments")
 public class PaymentsController {
-
-
-    private final MongoPaymentRepositoryAdapter mongoPaymentRepositoryAdapter;
+    private final PaymentRepository mongoPaymentRepositoryAdapter;
 
     @PostMapping("")
     public ResponseEntity<Payment> sendPayment(@RequestBody Payment payment){
         var entity = mongoPaymentRepositoryAdapter.save(payment);
         return  ResponseEntity.ok(entity);
     }
-
 }
